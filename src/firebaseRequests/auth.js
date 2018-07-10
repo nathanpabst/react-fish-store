2017a0c56968aff2f3eb96b9210bc44cd46d939a
Nathan Pabst <nathan.pabst@gmail.com>
2018-07-09 20:51:29 -0500

    firebase get request

--- src/firebaseRequests/auth.js
+++ src/firebaseRequests/auth.js
@@ -12,4 +12,8 @@ const logoutUser = () => {
   return firebase.auth().signOut();
 };
 
-export default {registerUser, loginUser, logoutUser};
+const getUid = () => {
+  return firebase.auth().currentUser.uid;
+};
+
+export default {registerUser, loginUser, logoutUser, getUid};
