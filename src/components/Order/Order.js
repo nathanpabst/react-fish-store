bb9f996f88681cb03c47de3e157f4948b9080100
Nathan Pabst <nathan.pabst@gmail.com>
2018-07-10 18:46:29 -0500

    order section formatted

--- src/components/Order/Order.js
+++ src/components/Order/Order.js
@@ -1,12 +1,39 @@
 import React from 'react';
 
+import formatPrice from '../../helpers';
+
 import './Order.css';
 
 class Order extends React.Component {
+  renderOrder = (key) => {
+    const fish = this.props.fishes.find(x => x.id === key);
+    const count = this.props.orders[key];
+
+    return (
+      <li
+        key={key}
+        className="text-left"
+      >
+        <div className="col-xs-2 count">{count} lbs</div>
+        <div className="col-xs-5">{fish.name}</div>
+        <div className="col-xs-3">{formatPrice(fish.price)}</div>
+        <div className="col-xs-2">
+          <button className="btn btn-default">&times;</button>
+        </div>
+      </li>
+    );
+  }
   render () {
+    const orderIds = Object.keys(this.props.orders);
+
     return (
       <div className="Order">
-        <h1>Order</h1>
+        <h2>Order</h2>
+        <ul>
+          {orderIds.map(this.renderOrder)}
+        </ul>
+        <div className="total">Total:</div>
+        <button className="btn btn-default">Save Order</button>
       </div>
     );
   }
